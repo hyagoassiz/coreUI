c4494555ff22e7b349c8d5dd1448a39f4d40a47f
hyagoassiz <hyagoassiz@hotmail.com>
2025-03-22 13:54:40 -0300

    PIZ-1 refactor(PizzariaX): instalando lib do material ui

--- src/pages/Solicitacao/Pizzaria/Pizzaria.tsx
+++ src/pages/Solicitacao/Pizzaria/Pizzaria.tsx
@@ -1,11 +1,3 @@
-import { useParams } from "react-router-dom";
-
 export const Pizzaria: React.FC = () => {
-  const { id } = useParams();
-
-  return (
-    <div className="bg-amber-300 justify-items-center">
-      <h1 className="text-blue-500 uppercase">Pizzaria: {id} </h1>
-    </div>
-  );
+  return <>Olá</>;
 };
