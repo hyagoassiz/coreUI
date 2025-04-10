9936e9ef14b125cef4ab417195f7a53a73cb2b1c
hyagoassiz <hyagoassiz@hotmail.com>
2025-04-10 19:28:07 -0300

    COR-1 feat(PageTitle): adicionado componente

--- src/pages/News/Route1/Route1.tsx
+++ src/pages/News/Route1/Route1.tsx
@@ -1,1 +1,10 @@
-export const Route1: React.FC = () => <>News - Route 1</>;
+import { PageTitle } from "../../../components/PageTitle";
+
+export const Route1: React.FC = () => (
+  <>
+    <PageTitle
+      title="Route 1"
+      subTitle="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+    />
+  </>
+);
